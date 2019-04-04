5771c5083e5f34dbbd3e1262ccc08afbe6bde248
u-ju <960316369@qq.com>
2019-04-04 10:57:16 +0800

    首页配色

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,12 +18,12 @@ Page({
       { image: '../../images/index.jpg' },
     ],
     tag:[
-      { name: "空调维修", icon: 'icon-weixiubeijing'},
-      { name: "空调清洗", icon: 'icon-xifashui' },
-      { name: "空调安装", icon: 'icon-xiyiji'},
-      { name: "空调移机", icon: 'icon-banjiapeisong'},
-      { name: "空调保养", icon: 'icon-jiajubaoyang'},
-      { name: "空调品牌", icon: 'icon-pinpaitemai-tianchong'},
+      { name: "空调维修", icon: 'icon-weixiubeijing', class:'jb1'},
+      { name: "空调清洗", icon: 'icon-xifashui', class: 'jb2' },
+      { name: "空调安装", icon: 'icon-xiyiji', class: 'jb3'},
+      { name: "空调移机", icon: 'icon-banjiapeisong', class: 'jb4'},
+      { name: "空调保养", icon: 'icon-jiajubaoyang', class: 'jb5'},
+      { name: "空调品牌", icon: 'icon-pinpaitemai-tianchong', class: 'jb6'},
     ]
   },
   
