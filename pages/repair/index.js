f3f8f640e70be566f1c3b5ad6a518a7f1de0cce9
u-ju <960316369@qq.com>
2019-04-11 19:49:15 +0800

    Revert "详情锚点"
    This reverts commit 63dd40a06397eae64ad5fd6e114e63f52b8dbd1a.

--- pages/repair/index.js
+++ pages/repair/index.js
@@ -6,7 +6,7 @@ Page({
     visible2: false,
     scrollTop: 0,
     last_scrollTop: 0,
-    toView: 'page0',
+    toView: 0,
     navActive: 0,
     lastActive: 0,
     s_height: '',
@@ -30,43 +30,59 @@ Page({
     ]
   },
   tap: function (e) {
-    console.log(e)
-    // var id = e.currentTarget.dataset.id;
+    var id = e.currentTarget.dataset.id;
     var index = e.currentTarget.dataset.index;
     this.setData({
-      toView: 'page-' + index,
+      toView: id,
       navActive: index
     });
-    console.log(this.data.toView)
+  },
+  scroll: function (e) {
+    var self = this;
+
+    //self.setData({scrollTop:e.detail.scrollTop});
+    //console.log("sd:",self.data.scrollTop);
+
+
+    //setTimeout(function(){
+
+    //if(self.data.last_scrollTop!=self.data.scrollTop){
+    //console.log(self.data.last_scrollTop);
+    //self.setData({last_scrollTop:self.data.scrollTop});
+    self.scrollmove(self, e, e.detail.scrollTop);
+    //}
+    // },1000);
+
+  },
+  scrollmove: function (self, e, scrollTop) {
+    // last_scrollTop=scrollTop;
+    var scrollArr = self.data.height_arr;
+    if (scrollTop > scrollArr[scrollArr.length - 1] - self.data.s_height) {
+      return;
+    } else {
+      for (var i = 0; i < scrollArr.length; i++) {
+        if (scrollTop >= 0 && scrollTop < scrollArr[0]) {
+          if (0 != self.data.lastActive) {
+            self.setData({
+              navActive: 0,
+              lastActive: 0
+            });
+          }
+        } else if (scrollTop >= scrollArr[i - 1] && scrollTop <= scrollArr[i]) {
+          if (i != self.data.lastActive) {
+            self.setData({
+              navActive: i,
+              lastActive: i
+            });
+          }
+        }
+      }
+    }
   },
   onLoad: function () {
-    // var s_height = wx.getSystemInfoSync().windowHeight;
-    // this.setData({ s_height: s_height });
-    // this.getHeightArr(this);
-    var height=0,that = this,allheight=[]
-    wx.createSelectorQuery().selectAll('.page0').boundingClientRect(function (rect) {
-      allheight.push(height)
-      height = height + rect[0].height
-      
-    }).exec() 
-    wx.createSelectorQuery().selectAll('.page1').boundingClientRect(function (rect) {
-      allheight.push(height)
-      height = height + rect[0].height
-      
-    }).exec() 
-    wx.createSelectorQuery().selectAll('.page2').boundingClientRect(function (rect) {
-      allheight.push(height)
-      height = height + rect[0].height
-    }).exec() 
-    wx.createSelectorQuery().selectAll('.page3').boundingClientRect(function (rect) {
-      allheight.push(height)
-      height = height + rect[0].height
-      that.setData({
-        s_height: height,
-        allheight: allheight
-      })
-    }).exec() 
-    
+    var s_height = wx.getSystemInfoSync().windowHeight;
+    this.setData({ s_height: s_height });
+    this.getHeightArr(this);
   },
   getHeightArr: function (self) {
     var height = 0, height_arr = [], details = self.data.detail, s_height = self.data.s_height;
